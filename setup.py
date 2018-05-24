9ea98255ae594641fa2452c246040fded3480fbc
Juliano Murari <juliano.murari@maxiv.lu.se>
2018-05-24 17:10:49 +0000

    Add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+ 
+setup(name = "python-pandaboxlib",
+      version = "0.0.1",
+      description = ("Library for communication with PandABox." 
+                     "This communication is based in TCP/IP sockets."),
+      author = "Jens Sundberg/Juliano Murari",
+      author_email = "kitscontrols@maxiv.lu.se",
+      license = "GPLv3",
+      url = "http://www.maxlab.lu.se",
+      packages =['pandaboxlib'],
+      package_dir = {'':'src'},
+     )
