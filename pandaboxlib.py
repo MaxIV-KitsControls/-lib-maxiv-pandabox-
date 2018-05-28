fb7a5e894624bcb4585630dfbd9a5abdb54d0576
Jens Sundberg <jens.sundberg@maxiv.lu.se>
2018-05-28 10:19:03 +0200

    added library file

---
+++ pandaboxlib.py
@@ -0,0 +1,58 @@
+"""
+Class for the Pandabox communication.
+Class has no Tango dependence.
+KITS @ MAX-IV 2018-05-25.
+"""
+
+
+import socket
+import sys
+from save_config import get_lines, read_response, save_state, save_table
+from save_config import save_metatable, save_metadata
+
+class PandA:
+   
+ 
+    def __init__(self, host):
+        self.host = host
+        self.port = 8888
+         
+        # Create a TCP socket 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+        self.sock.settimeout(1)
+        
+    def connect_to_panda(self):
+        self.sock.connect((self.host, self.port))
+
+    def disconnect_from_panda(self):
+        self.sock.shutdown(socket.SHUT_WR)
+        self.sock.close()
+        
+    def query(self, cmd):
+        self.sock.sendall(cmd + '\n')
+        val = str(self.sock.recv(4096))
+        print(val)
+
+    def save_config(self, save_file):
+        input = get_lines(self.sock)
+        output = file(save_file, 'w')
+        
+        # First save the CONFIG state
+        save_state(input, output, '*CHANGES.ATTR?', self.sock)
+        save_state(input, output, '*CHANGES.CONFIG?', self.sock)
+        # Now save the individual tables.
+        # Note that we must read the complete table response before processing!
+        tables = list(read_response(input, '*CHANGES.TABLE?', self.sock))
+        for table in tables:
+            save_table(input, output, table, self.sock)
+         
+        # Finally the metadata is a bit more tricky, because the table and value results
+        # are mixed together.
+        metadata = list(read_response(input, '*CHANGES.METADATA?', self.sock))
+        for line in metadata:
+            save_metadata(input, output, line, self.sock)
+
+    def load_config(self, load_file):
+        for line in open(load_file):
+            self.sock.sendall(line)
---
+++ save_config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+
+# Script to save the state of the given Panda.
+
+import socket
+
+
+# Reads response lines from config socket.
+class get_lines:
+    def __init__(self, sock):
+        self.sock = sock
+        self.buf = ''
+        self.lines = []
+
+    def __iter__(self):
+        return self
+
+    def __read_lines(self, buf):
+        while True:
+            rx = self.sock.recv(65536)
+            if not rx:
+                raise StopIteration
+            buf += rx
+
+            lines = buf.split('\n')
+            if len(lines) > 1:
+                break
+
+        return lines[0], lines[1:-1], lines[-1]
+
+
+    def next(self):
+        if self.lines:
+            line = self.lines[0]
+            del self.lines[0]
+        else:
+            line, self.lines, self.buf = self.__read_lines(self.buf)
+        return line
+
+
+def read_response(input, command, sock):
+    sock.sendall(command + '\n')
+    for line in input:
+        if line[0] == '!':
+            yield line[1:]
+        elif line[0] == '.':
+            break
+        else:
+            assert False, 'Malformed response: "%s"' % line
+
+def save_state(input, output, command, sock):
+    for line in read_response(input, command, sock):
+        print >>output, line
+
+
+def save_table(input, output, table, sock):
+    assert table[-1] == '<'
+    print >>output, table + 'B'
+    for line in read_response(input, table[:-1] + '.B?', sock):
+        print >>output, line
+    print >>output
+
+
+def save_metatable(input, output, table, sock):
+    print >>output, table
+    for line in read_response(input, table[:-1] + '?', sock):
+        print >>output, line
+    print >>output
+
+def save_metadata(input, output, line, sock):
+    if line[-1] == '<':
+        save_metatable(input, output, line, sock)
+    else:
+        print >>output, line
