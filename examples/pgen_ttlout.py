f95eec2c7cd1cba6f23eab3babca34a31a6de27b
Juliano Murari <juliano.murari@maxiv.lu.se>
2019-03-04 16:55:52 +0100

    Update example with the new software

--- examples/pgen_ttlout.py
+++ examples/pgen_ttlout.py
@@ -6,13 +6,13 @@ host = 'pandabox'
 panda = PandA(host)
 panda.connect_to_panda()
 
-panda.query('CLOCKS.A_PERIOD=.1')
+panda.query('CLOCKS.A_PERIOD=.05')
 panda.query('CLOCKS.A_PERIOD.UNITS=s')
 
 panda.query('PGEN1.TRIG=CLOCKS.OUTA')
 panda.query('PGEN1.CYCLES=1')
 
-num_pts = 201
+num_pts = 1001
 pos_i = 0
 pos_f = 1000
 pos = np.linspace(pos_i, pos_f, num=num_pts)
@@ -22,19 +22,25 @@ print pos_cmd
 
 panda.query('PGEN1.TABLE<\n'+''.join(pos_cmd))
 panda.query('PCOMP1.INP=PGEN1.OUT')
-panda.query('PCOMP1.START=0')
+panda.query('PCOMP1.START=10')
+
 step_size = 10 
 pnum = (pos_f - pos_i) / step_size
-panda.query('PCOMP1.STEP=%d' % (step_size))
 print 'pnum: ', pnum
-panda.query('PCOMP1.PNUM=%d' % (pnum))
+
+panda.query('PCOMP1.PRE_START=-1')
+panda.query('PCOMP1.STEP=%d' % (step_size))
+panda.query('PCOMP1.PULSES=%d' % (pnum))
+panda.query('PCOMP1.WIDTH=1')
 panda.query('PCOMP1.RELATIVE=Absolute')
 panda.query('PCOMP1.DIR=Positive')
 
 panda.query('TTLOUT1.VAL=PCOMP1.OUT')
 
 
-panda.query('PCOMP1.ENABLE=BITS.ZERO')
-panda.query('PCOMP1.ENABLE=BITS.ONE')
-panda.query('PGEN1.ENABLE=BITS.ONE')
+panda.query('PCOMP1.ENABLE=ZERO')
+panda.query('PCOMP1.ENABLE=ONE')
+panda.query('PGEN1.ENABLE=ONE')
+panda.query('COUNTER1.ENABLE=ZERO')
+panda.query('COUNTER1.ENABLE=ONE')
 
