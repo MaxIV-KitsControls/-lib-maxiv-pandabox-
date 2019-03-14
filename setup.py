99c86abaf7048350fab165cfbedd12cfb4732606
Juliano Murari <juliano.murari@maxiv.lu.se>
2019-03-14 10:44:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
  
 setup(name = "python-pandaboxlib",
-      version = "0.0.1",
+      version = "0.0.2",
       description = ("Library for communication with PandABox." 
                      "This communication is based in TCP/IP sockets."),
       author = "Jens Sundberg/Juliano Murari",
