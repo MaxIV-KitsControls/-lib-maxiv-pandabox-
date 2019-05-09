c44480e2c775241458e21865231569b4c9a95ff8
Juliano Murari <juliano.murari@maxiv.lu.se>
2019-05-09 17:54:30 +0200

    Add method to send positions to SEQ table

--- src/pandaboxlib/pandaboxlib.py
+++ src/pandaboxlib/pandaboxlib.py
@@ -70,3 +70,34 @@ class PandA:
     def load_config(self, load_file):
         for line in open(load_file):
             self.sock.sendall(line)
+
+    def send_seq_table(self, block_id, repeats, trigger, 
+                       positions, time1, phase1, time2, phase2):
+        """
+          Function to send an array of positions to the sequencer (SEQ)
+          table.
+        """
+        trigger_options = {'Immediate': 0, 'bita=0': 1, 'bita=1': 2, 'bitb=0': 3, 
+            'bitb=1': 4, 'bitc=0': 5, 'bitc=1': 6, 'posa>=position': 7,
+            'posa<=position': 8, 'posb>=position': 9, 'posb<=position': 10,
+            'posc>=position': 11, 'posc<=position': 12}
+
+        # _b binary code
+        repeats_b = '{0:016b}'.format(repeats)                 # 16 bits
+        trigger_b = '{0:04b}'.format(trigger_options[trigger]) # 4 bits (17-20)
+        phase1_b = ""
+        for key,value in sorted(phase1.iteritems()):           # 6 bits (a-f)
+            phase1_b = "1"+phase1_b if value else "0"+phase1_b
+        phase2_b = ""
+        for key,value in sorted(phase2.iteritems()):           # 6 bits (a-f)
+            phase2_b = "1"+phase2_b if value else "0"+phase2_b
+        code_b =  phase2_b+phase1_b+trigger_b+repeats_b          # 32 bits code
+        code = int(code_b,2)
+        
+        # table < code position time1 time2
+        pos_cmd = ['%d %d %d %d\n' % (code, pos, time1, time2) for pos in positions]
+        
+        # < overwirte; << append
+        #self.query('SEQ%d.TABLE<<\n'%(block_id)+''.join(pos_cmd))
+        self.query('SEQ%d.TABLE<\n'%(block_id)+''.join(pos_cmd))
+
