3c08401e2a39905849fb21772c752d5ce36379b4
Jens Sundberg <jens.sundberg@maxiv.lu.se>
2019-06-11 17:23:30 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
  
 setup(name = "python-pandaboxlib",
-      version = "0.0.3",
+      version = "0.0.4",
       description = ("Library for communication with PandABox." 
                      "This communication is based in TCP/IP sockets."),
       author = "Jens Sundberg/Juliano Murari",
