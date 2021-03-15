13fc18fb34e07bdda8d7704fbef12e1682ff4c12
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-03-15 15:02:14 +0100

    Add basic docstrings

--- src/pandaboxlib/pandaboxlib.py
+++ src/pandaboxlib/pandaboxlib.py
@@ -20,14 +20,21 @@ class _Design:
     """
 
     def __init__(self, sock):
+        """Initializer"""
         self.sock = sock
         self.buf = ''
         self.lines = []
 
     def __iter__(self):
+        """Iterator (self)"""
         return self
 
     def __read_lines(self, buf):
+        """Read multiple response lines from host as sequence
+
+        N.b. Only returns on _multiline_ responses!
+
+        """
         while True:
             bufsize = 2**16
             rx = str(self.sock.recv(bufsize).decode())
@@ -42,6 +49,7 @@ class _Design:
         return lines[0], lines[1:-1], lines[-1]
 
     def __next__(self):
+        """Iterate over response lines"""
         if self.lines:
             line = self.lines[0]
             del self.lines[0]
@@ -50,6 +58,11 @@ class _Design:
         return line
 
     def _read_response(self, command):
+        """Send command to host and read multi-value response
+
+        N.b. Raises error on single reponses!
+
+        """
         self.sock.sendall((command + '\n').encode())
         for line in self:
             if line[0] == '!':
@@ -60,6 +73,7 @@ class _Design:
                 assert False, 'Malformed response: "{line}"'.format(line)
 
     def _read_idn(self):
+        """Return identifcation string"""
         command = "*IDN?"
         self.sock.sendall(f"{command}\n".encode())
         bufsize = 2**16
@@ -70,10 +84,16 @@ class _Design:
         return response
 
     def _save_state(self, file, command):
+        """Save multi-value response from command to file"""
         for line in self._read_response(command):
             print(line, file=file)
 
     def _save_table(self, file, table):
+        """Save table field to file
+
+        N.b. Table values are base64 encoded
+
+        """
         assert table[-1] == '<'
         print(table + 'B', file=file)
         for line in self._read_response(table[:-1] + '.B?'):
@@ -81,12 +101,22 @@ class _Design:
         print('', file=file)
 
     def _save_metatable(self, file, table):
+        """Save table field to file
+
+        N.b. Table values are ASCII encoded
+
+        """
         print(table, file=file)
         for line in self._read_response(table[:-1] + '?'):
             print(line, file=file)
         print('', file=file)
 
     def _save_metadata(self, file, line):
+        """Save metadata to file
+
+        Metadata fields can be single or table types
+
+        """
         if line[-1] == '<':
             self._save_metatable(file, line)
         else:
@@ -137,6 +167,7 @@ class _Design:
         return fpga_design == fpga_installed
 
     def save(self, path):
+        """Save design to file"""
         with open(path, "wt") as file:
 
             # Save firmware version
@@ -163,6 +194,7 @@ class _Design:
                 self._save_metadata(file, line)
 
     def load(self, path):
+        """Load design from file"""
         with open(path,"rt") as file:
 
             # Validate FPGA firmware version
@@ -179,45 +211,61 @@ class PandA:
     sock = None
 
     def __init__(self, host, port=8888):
+        """Initializer"""
         self.host = host
         self.port = port
 
     def __del__(self):
+        """Finalizer/destructor
+
+        Actions:
+
+          * Close socket gracefully
+
+        """
         if self.sock is not None:
             self.disconnect_from_panda()
 
     def connect_to_panda(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(
-            socket.SOL_TCP,          # Disable Nagle algorithm
-            socket.TCP_NODELAY,      # (https://linux.die.net/man/7/tcp)
-            1                        # Is this really necessary?
-        )
-        self.sock.settimeout(1)
-        self.sock.connect((self.host, self.port))
+        """Create socket connection to host"""
+        if self.socket is None
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setsockopt(
+                socket.SOL_TCP,          # Disable Nagle algorithm
+                socket.TCP_NODELAY,      # (https://linux.die.net/man/7/tcp)
+                1                        # Is this really necessary?
+            )
+            self.sock.settimeout(1)
+            self.sock.connect((self.host, self.port))
 
     def disconnect_from_panda(self):
-        self.sock.shutdown(socket.SHUT_WR)
-        self.sock.close()
-        self.sock = None
+        """Close socket connection to host"""
+        if self.socket is not None
+            self.sock.shutdown(socket.SHUT_WR)
+            self.sock.close()
+            self.sock = None
 
     def query(self, cmd):
+        """Send command to host and return response"""
         self.sock.sendall((cmd + '\n').encode())
         val = str(self.sock.recv(4096).decode())
         logger.debug("query %s returned %s" % (cmd, str(val).strip()))
         return val
 
     def query_value(self, cmd):
+        """Send command to host and return response as float"""
         val = self.query(cmd)
         return float(val.strip("\n").split("=")[1])
 
     def _num(self, s):
+        """Cast value to int, else float"""
         try:
             return int(s)
         except ValueError:
             return float(s)
 
     def numquery(self, cmd):
+        """Send command to host and return response as int or float"""
         val = self.query(cmd)
         val = val.split("=")[-1]
         val = self._num(val)
@@ -225,20 +273,19 @@ class PandA:
         return val
 
     def save_config(self, path):
+        """Save design to file"""
         if self.sock is not None:
             self.disconnect_from_panda()
         self.connect_to_panda()              # Ensure first `*CHANGES` request on connection
         _Design(self.sock).save(path)
 
     def load_config(self, path):
+        """Load design from file"""
         _Design(self.sock).load(path)
 
     def send_seq_table(self, block_id, repeats, trigger,
                        positions, time1, phase1, time2, phase2):
-        """
-          Function to send an array of positions to the sequencer (SEQ)
-          table.
-        """
+        """Send array of positions to sequencer (SEQ) table"""
         trigger_options = {'Immediate': 0, 'bita=0': 1, 'bita=1': 2, 'bitb=0': 3,
                            'bitb=1': 4, 'bitc=0': 5, 'bitc=1': 6, 'posa>=position': 7,
                            'posa<=position': 8, 'posb>=position': 9, 'posb<=position': 10,
@@ -264,9 +311,7 @@ class PandA:
         self.query('SEQ%d.TABLE<\n' % (block_id) + ''.join(pos_cmd))
 
     def get_number_channels(self):
-        """
-        Returns the number of channels enabled in the capture.
-        """
+        """Return number of enabled capture channels"""
         capture_str = self.query('*CAPTURE?')
         capture_str = capture_str.split('\n')
         num_chan = (len(capture_str) - 2)  # -2 because of . and ""
