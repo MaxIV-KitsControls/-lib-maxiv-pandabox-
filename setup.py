94ed6f3c7bb7d1888a8fed0865398d438ad030b2
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-04-02 00:51:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from setuptools import setup
 
 
 setup(name = "pandaboxlib",
-      version = "1.0.3",
-      description = ("Library for communication with PandABox."
-                     "This communication is based in TCP/IP sockets."),
-      author = "Jens Sundberg/Juliano Murari",
+      description = ("Library for socket-based communication with PandABox."),
+      author = "KITS, MAX IV Laboratory",
       author_email = "kitscontrols@maxiv.lu.se",
       license = "GPLv3",
       url = "https://github.com/MaxIV-KitsControls/lib-maxiv-pandaboxlib",
       packages =['pandaboxlib'],
-      package_dir = {'':'src'},
+      package_dir = {'':'pandaboxlib'}
      )
